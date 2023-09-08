0f020a1ece6729151ac304f2654c9fdb159adb44
Tran Phu Son <phuson2809@gmail.com>
2023-09-08 23:12:21 +0700

    fix(code base): edit folder structure

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,8 @@
 import Input from './input/Input';
 import Logo from './logo/Logo';
+import Label from './label/Label';
 import NavSection from './nav-section/NavSection';
 import ScrollToTop from './scroll-to-top/ScrollToTop';
 import Scrollbar from './scrollbar/Scrollbar';
 
-export { Input, Logo, NavSection, ScrollToTop, Scrollbar };
+export { Input, Logo, Label, NavSection, ScrollToTop, Scrollbar };
