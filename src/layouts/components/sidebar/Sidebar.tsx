31ec6a5852e68c6a75a90ac8eea3c8a42005e493
VoVyKhang <vo.vykhang24@gmail.com>
2023-09-14 12:41:13 +0700

    build(admin): add list kitchen center page

--- src/layouts/components/sidebar/Sidebar.tsx
+++ src/layouts/components/sidebar/Sidebar.tsx
@@ -50,7 +50,7 @@ function Sidebar({ openNav, onCloseNav }: SidebarProps) {
         </Box>
 
         <Box width="100%">
-          {navAdmin.map((navItem, index) => (
+          {navBrand.map((navItem, index) => (
             <Box key={index} mb={1}>
               <Typography
                 sx={{
