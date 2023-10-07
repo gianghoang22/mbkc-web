d618561b2c2e34c0a0d7ec16e66820078c26b174
VoVyKhang <vo.vykhang24@gmail.com>
2023-10-07 11:30:17 +0700

    fix(profile): fix file structure

--- src/pages/profile/InformationPage.tsx
+++ src/pages/profile/InformationPage.tsx
@@ -1,67 +1,68 @@
-import React from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 // mui
 import { Box, Card, Paper, Stack, Grid } from '@mui/material';
 
-import { Page } from 'components';
 import { brand } from '../../mock/brand';
 import { kitchenCenter } from '../../mock/kitchenCenter';
-import { PATH_KITCHEN_CENTER_APP } from 'routes/paths';
 import ItemInformation from './ItemInformation';
-import ProfileDetail from './ProfileDetail';
 import { useAppSelector } from 'redux/configStore';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { getKitchenCenterProfile } from 'redux/profile/profileSlice';
+import { getBrandProfile, getKitchenCenterProfile } from 'redux/profile/profileSlice';
+import { InformationPageDetail, InformationPageDetailSkeleton } from '.';
+import { Role } from 'common/enum';
 
 function InformationPage() {
-  const { pathname } = useLocation();
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const { profile } = useAppSelector((state) => state.profile);
+  const { profile, isLoading } = useAppSelector((state) => state.profile);
+  const { userAuth } = useAppSelector((state) => state.auth);
 
   useEffect(() => {
-    dispatch<any>(getKitchenCenterProfile(navigate));
-  }, []);
+    if (userAuth?.roleName === Role.BRAND_MANAGER) {
+      dispatch<any>(getBrandProfile(navigate));
+    }
+
+    if (userAuth?.roleName === Role.KITCHEN_CENTER_MANAGER) {
+      dispatch<any>(getKitchenCenterProfile(navigate));
+    }
+  }, [userAuth.roleName, dispatch, navigate]);
 
   return (
     <>
-      <Page title="Profile" pathname={pathname} navigateDashboard={PATH_KITCHEN_CENTER_APP.root}>
-        <Card>
-          <Box sx={{ width: '100%' }} paddingLeft={2} paddingRight={2}>
-            <Paper sx={{ width: '100%', mb: 2 }}>
-              <Stack borderRadius={2} width="100%" bgcolor="#000" flexDirection="row" justifyContent="center">
-                <Box
-                  marginTop={-1}
-                  width={200}
-                  component="img"
-                  src="/assets/images/logos/logo_mbkc_no_background.png"
-                />
-              </Stack>
+      <Card>
+        <Box sx={{ width: '100%' }} paddingLeft={2} paddingRight={2}>
+          <Paper sx={{ width: '100%', mb: 2 }}>
+            <Stack borderRadius={2} width="100%" bgcolor="#000" flexDirection="row" justifyContent="center">
+              <Box marginTop={-1} width={200} component="img" src="/assets/images/logos/logo_mbkc_no_background.png" />
+            </Stack>
 
-              <ProfileDetail
+            {isLoading ? (
+              <InformationPageDetailSkeleton />
+            ) : (
+              <InformationPageDetail
                 logo={profile?.logo}
                 address={profile?.address}
                 managerEmail={profile?.kitchenCenterManagerEmail}
                 name={profile?.name}
                 status={profile?.status}
               />
-            </Paper>
-          </Box>
-        </Card>
+            )}
+          </Paper>
+        </Box>
+      </Card>
 
-        <Grid container columnSpacing={5} rowSpacing={5} mt={0.1}>
-          <Grid item xs={12} sm={6} md={6}>
-            <ItemInformation isKitchenCenter={false} brand={brand} />
-          </Grid>
-
-          <Grid item xs={12} sm={6} md={6}>
-            <ItemInformation isKitchenCenter kitchenCenter={kitchenCenter} />
-          </Grid>
+      <Grid container columnSpacing={5} rowSpacing={5} mt={0.1}>
+        <Grid item xs={12} sm={6} md={6}>
+          <ItemInformation isKitchenCenter={false} brand={brand} />
         </Grid>
-      </Page>
+
+        <Grid item xs={12} sm={6} md={6}>
+          <ItemInformation isKitchenCenter kitchenCenter={kitchenCenter} />
+        </Grid>
+      </Grid>
     </>
   );
 }
--- src/pages/profile/index.ts
+++ src/pages/profile/index.ts
@@ -1,2 +1,5 @@
 export { default as ProfilePage } from './ProfilePage';
 export { default as InformationPage } from './InformationPage';
+export { default as InformationPageDetail } from './InformationPageDetail';
+export { default as InformationPageDetailSkeleton } from './InformationPageDetailSkeleton';
+export { default as ItemInformation } from './ItemInformation';
