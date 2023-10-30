f28304ce40d3af3296b950eec20cda8b656be5fd
VoVyKhang <vo.vykhang24@gmail.com>
2023-10-30 12:02:03 +0700

    feat(dashboard): fix ui admin dashboard

--- src/sections/brand/BrandTableRowDashboardSkeleton.tsx
+++ src/sections/brand/BrandTableRowDashboardSkeleton.tsx
@@ -1,26 +1,24 @@
-import { Skeleton, TableCell, TableRow, Typography } from '@mui/material';
+import { Skeleton, TableCell, TableRow } from '@mui/material';
 
 function BrandTableRowDashboardSkeleton({ length = 3 }: { length: number }) {
   return (
     <>
       {Array.from({ length }).map((_, index) => (
         <TableRow key={index} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-          <TableCell component="th" scope="row">
+          <TableCell width={60} component="th" scope="row">
             <Skeleton />
           </TableCell>
           <TableCell>
             <Skeleton variant="circular" width={40} height={40} />
           </TableCell>
           <TableCell>
-            <Typography variant="subtitle2" style={{ marginLeft: 4, fontWeight: 600 }}>
-              <Skeleton width={100} />
-            </Typography>
-          </TableCell>
-          <TableCell>
             <Skeleton width={100} />
           </TableCell>
           <TableCell>
-            <Skeleton width={60} />
+            <Skeleton width={600} />
+          </TableCell>
+          <TableCell>
+            <Skeleton width={60} height={30} />
           </TableCell>
         </TableRow>
       ))}
